92d609cf50f26dad069ffd68e7295e4ed9d37596
Adao Natalino <sansonov@gmail.com>
2020-04-06 15:04:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,7 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round
+def initial_round(sum)
   a = deal_card
   b = deal_card
   display_card_total
